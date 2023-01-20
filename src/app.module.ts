aa93c252d398b8d7dd2d0fd1c084cd4835b74216
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-01-20 15:03:19 -0300

    validacao notificacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MailService } from './mail/mail.service';
-import { PostmarkMailService } from './mail/postmark-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
-    {
-      provide: MailService,
-      useClass: PostmarkMailService,
-    }
+    PrismaService,
   ],
 })
 export class AppModule {}
